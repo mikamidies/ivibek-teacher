72c312cab23e9c23945495239c61ce779853ef76
mikamidies <mikamidies04@gmail.com>
2025-10-24 12:46:55 +0500

    Update

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -36,29 +36,40 @@ const handleLogin = async () => {
 <template>
   <div class="login-page auth">
     <div class="login__wrapper">
-      <div class="login__logo">
-        <NuxtImg src="images/brand.svg" alt="Logo" width="120" height="40" />
+      <div class="login__img">
+        <NuxtImg
+          src="/images/login.svg"
+          alt="Register Illustration"
+          width="500"
+          height="500"
+        />
+        <p class="login__type">Platform for Teachers</p>
       </div>
-      <div class="login__body">
-        <div class="login__header">
-          <h4 class="login__title">Log in</h4>
-          <p class="login__sub">Welcome back please enter your details.</p>
-        </div>
-        <form class="login__form" @submit.prevent="handleLogin">
-          <a-input
-            v-model:value="username"
-            placeholder="Username"
-            class="login__input"
-            :disabled="loading"
-          />
-          <a-input-password
-            v-model:value="password"
-            placeholder="Password"
-            class="login__input"
-            :disabled="loading"
-          />
 
-          <!-- <div class="login__flexer">
+      <div class="login__body">
+        <div class="login__logo">
+          <NuxtImg src="images/brand.svg" alt="Logo" width="120" height="40" />
+        </div>
+        <div class="login__somewhat">
+          <div class="login__header">
+            <h4 class="login__title">Log in</h4>
+            <p class="login__sub">Welcome back please enter your details.</p>
+          </div>
+          <form class="login__form" @submit.prevent="handleLogin">
+            <a-input
+              v-model:value="username"
+              placeholder="Username"
+              class="login__input"
+              :disabled="loading"
+            />
+            <a-input-password
+              v-model:value="password"
+              placeholder="Password"
+              class="login__input"
+              :disabled="loading"
+            />
+
+            <!-- <div class="login__flexer">
             <a-checkbox v-model:checked="remember" class="login__checkbox">
               Remember me
             </a-checkbox>
@@ -66,23 +77,24 @@ const handleLogin = async () => {
               Forgot Password?
             </NuxtLink>
           </div> -->
-          <div class="login__buttons">
-            <a-button
-              type="primary"
-              class="login__btn"
-              html-type="submit"
-              :loading="loading"
-            >
-              Log in
-            </a-button>
-            <div class="login__link">
-              <p>Don't have an account?</p>
-              <NuxtLink to="/auth/register" class="login__register">
-                Sign up
-              </NuxtLink>
+            <div class="login__buttons">
+              <a-button
+                type="primary"
+                class="login__btn"
+                html-type="submit"
+                :loading="loading"
+              >
+                Log in
+              </a-button>
+              <div class="login__link">
+                <p>Don't have an account?</p>
+                <NuxtLink to="/auth/register" class="login__register">
+                  Sign up
+                </NuxtLink>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -92,30 +104,70 @@ const handleLogin = async () => {
 .login-page {
   display: flex;
   justify-content: center;
-  align-items: center;
   height: 100vh;
   background: #f3f4f6;
 }
 .login__wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 24px;
+  padding: 40px;
   width: 100%;
-  max-width: 465px;
+}
+.login__img {
   display: flex;
-  flex-direction: column;
   align-items: center;
-  gap: 80px;
+  justify-content: center;
+  flex-direction: column;
+  gap: 140px;
+  background: var(--blue);
+  border-radius: 24px;
+  box-shadow: 0px 0px 34.8px 10px #ffffff8f inset;
+}
+.login__type {
+  font-weight: 500;
+  font-style: Medium;
+  font-size: 24px;
+  line-height: 100%;
+  letter-spacing: 0%;
+  text-align: center;
+  text-transform: uppercase;
+  color: white;
+  letter-spacing: 1px;
+}
+.login__img img {
+  width: 480px;
+  height: auto;
+  object-fit: contain;
+  object-position: center;
+  mix-blend-mode: luminosity;
 }
 .login__body {
   width: 100%;
+  height: 100%;
   background: #ffffff;
   border-radius: 24px;
   padding: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.login__logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 24px;
 }
 .login__logo img {
   width: 292px;
   height: 98px;
   object-fit: contain;
   object-position: center;
-  margin-top: -100px;
+}
+.login__somewhat {
+  min-width: 480px;
+  max-width: 90%;
+  margin: 0 auto;
 }
 .login__header {
   margin-bottom: 16px;
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -92,164 +92,179 @@ const handleRegister = async () => {
 <template>
   <div class="login-page auth">
     <div class="login__wrapper">
-      <div class="login__logo">
-        <NuxtImg src="/images/brand.svg" alt="Logo" width="120" height="40" />
+      <div class="login__img">
+        <NuxtImg
+          src="/images/login.svg"
+          alt="Register Illustration"
+          width="500"
+          height="500"
+        />
+        <p class="login__type">Platform for Teachers</p>
       </div>
       <div class="login__body">
-        <div class="login__header">
-          <h4 class="login__title">Register</h4>
-          <p class="login__sub">
-            Create your account. It's free and only takes a minute.
-          </p>
+        <div class="login__logo">
+          <NuxtImg src="/images/brand.svg" alt="Logo" width="120" height="40" />
         </div>
-        <form class="login__form" @submit.prevent="handleRegister">
-          <a-input
-            v-model:value="username"
-            placeholder="Username"
-            class="login__input"
-            :disabled="loading"
-          />
-          <a-input
-            v-model:value="fullName"
-            placeholder="Full Name"
-            class="login__input"
-            :disabled="loading"
-          />
-          <a-input
-            v-model:value="email"
-            type="email"
-            placeholder="Email"
-            class="login__input"
-            :disabled="loading"
-          />
-          <a-select
-            v-model:value="gender"
-            placeholder="Gender"
-            class="login__input"
-            :disabled="loading"
-          >
-            <a-select-option value="MALE">Male</a-select-option>
-            <a-select-option value="FEMALE">Female</a-select-option>
-          </a-select>
-
-          <a-select
-            v-model:value="countryId"
-            show-search
-            placeholder="Select Country"
-            class="login__input"
-            :disabled="loading"
-            :filter-option="
-              (input, option) =>
-                option.label.toLowerCase().includes(input.toLowerCase())
-            "
-          >
-            <a-select-option
-              v-for="country in countries"
-              :key="country.id"
-              :value="country.id"
-              :label="country.name"
-            >
-              {{ country.name }}
-            </a-select-option>
-          </a-select>
-
-          <a-date-picker
-            v-model:value="dateOfBirth"
-            placeholder="Date of Birth"
-            class="login__input"
-            :disabled="loading"
-            format="DD/MM/YYYY"
-            style="width: 100%"
-          />
-
-          <a-input-number
-            v-model:value="meetingHourPrice"
-            placeholder="Meeting Hour Price"
-            class="login__input"
-            :disabled="loading"
-            :min="0"
-            style="width: 100%"
-          />
-
-          <a-select
-            v-model:value="universityId"
-            show-search
-            placeholder="Select University"
-            class="login__input"
-            :disabled="loading"
-            :filter-option="
-              (input, option) =>
-                option.label.toLowerCase().includes(input.toLowerCase())
-            "
-            allow-clear
-          >
-            <a-select-option
-              v-for="university in universities"
-              :key="university.id"
-              :value="university.id"
-              :label="university.name"
-            >
-              {{ university.name }}
-            </a-select-option>
-          </a-select>
-
-          <a-select
-            v-model:value="majorId"
-            show-search
-            placeholder="Select Faculty"
-            class="login__input"
-            :disabled="loading"
-            :filter-option="
-              (input, option) =>
-                option.label.toLowerCase().includes(input.toLowerCase())
-            "
-            allow-clear
-          >
-            <a-select-option
-              v-for="faculty in faculties"
-              :key="faculty.id"
-              :value="faculty.id"
-              :label="faculty.name"
-            >
-              {{ faculty.name }}
-            </a-select-option>
-          </a-select>
-
-          <a-input-password
-            v-model:value="password"
-            placeholder="Password"
-            class="login__input"
-            :disabled="loading"
-          />
-          <a-input-password
-            v-model:value="confirmPassword"
-            placeholder="Confirm Password"
-            class="login__input"
-            :disabled="loading"
-          />
-
-          <div class="login__flexer">
-            <a-checkbox v-model:checked="agree" class="login__checkbox">
-              I agree to terms
-            </a-checkbox>
+        <div class="login__somewhat">
+          <div class="login__header">
+            <h4 class="login__title">Register</h4>
+            <p class="login__sub">
+              Create your account. It's free and only takes a minute.
+            </p>
           </div>
-          <div class="login__buttons">
-            <a-button
-              type="primary"
-              class="login__btn"
-              html-type="submit"
-              :loading="loading"
-            >
-              Register
-            </a-button>
-            <div class="login__link">
-              <p>Already have an account?</p>
-              <NuxtLink to="/auth/login" class="login__register">
-                Sign in
-              </NuxtLink>
+          <form class="login__form" @submit.prevent="handleRegister">
+            <div class="login__items">
+              <a-input
+                v-model:value="username"
+                placeholder="Username"
+                class="login__input"
+                :disabled="loading"
+              />
+              <a-input
+                v-model:value="fullName"
+                placeholder="Full Name"
+                class="login__input"
+                :disabled="loading"
+              />
+              <a-input
+                v-model:value="email"
+                type="email"
+                placeholder="Email"
+                class="login__input"
+                :disabled="loading"
+              />
+              <a-select
+                v-model:value="gender"
+                placeholder="Gender"
+                class="login__input"
+                :disabled="loading"
+              >
+                <a-select-option value="MALE">Male</a-select-option>
+                <a-select-option value="FEMALE">Female</a-select-option>
+              </a-select>
+
+              <a-select
+                v-model:value="countryId"
+                show-search
+                placeholder="Select Country"
+                class="login__input"
+                :disabled="loading"
+                :filter-option="
+                  (input, option) =>
+                    option.label.toLowerCase().includes(input.toLowerCase())
+                "
+              >
+                <a-select-option
+                  v-for="country in countries"
+                  :key="country.id"
+                  :value="country.id"
+                  :label="country.name"
+                >
+                  {{ country.name }}
+                </a-select-option>
+              </a-select>
+
+              <a-date-picker
+                v-model:value="dateOfBirth"
+                placeholder="Date of Birth"
+                class="login__input"
+                :disabled="loading"
+                format="DD/MM/YYYY"
+                style="width: 100%"
+              />
+
+              <a-input-number
+                v-model:value="meetingHourPrice"
+                placeholder="Meeting Hour Price"
+                class="login__input"
+                :disabled="loading"
+                :min="0"
+                style="width: 100%"
+              />
+
+              <a-select
+                v-model:value="universityId"
+                show-search
+                placeholder="Select University"
+                class="login__input"
+                :disabled="loading"
+                :filter-option="
+                  (input, option) =>
+                    option.label.toLowerCase().includes(input.toLowerCase())
+                "
+                allow-clear
+              >
+                <a-select-option
+                  v-for="university in universities"
+                  :key="university.id"
+                  :value="university.id"
+                  :label="university.name"
+                >
+                  {{ university.name }}
+                </a-select-option>
+              </a-select>
+
+              <a-select
+                v-model:value="majorId"
+                show-search
+                placeholder="Select Faculty"
+                class="login__input"
+                :disabled="loading"
+                :filter-option="
+                  (input, option) =>
+                    option.label.toLowerCase().includes(input.toLowerCase())
+                "
+                allow-clear
+              >
+                <a-select-option
+                  v-for="faculty in faculties"
+                  :key="faculty.id"
+                  :value="faculty.id"
+                  :label="faculty.name"
+                >
+                  {{ faculty.name }}
+                </a-select-option>
+              </a-select>
+
+              <a-input-password
+                v-model:value="password"
+                placeholder="Password"
+                class="login__input"
+                :disabled="loading"
+              />
+              <a-input-password
+                v-model:value="confirmPassword"
+                placeholder="Confirm Password"
+                class="login__input"
+                :disabled="loading"
+              />
             </div>
-          </div>
-        </form>
+
+            <div>
+              <div class="login__flexer">
+                <a-checkbox v-model:checked="agree" class="login__checkbox">
+                  I agree to terms
+                </a-checkbox>
+              </div>
+              <div class="login__buttons">
+                <a-button
+                  type="primary"
+                  class="login__btn"
+                  html-type="submit"
+                  :loading="loading"
+                >
+                  Register
+                </a-button>
+                <div class="login__link">
+                  <p>Already have an account?</p>
+                  <NuxtLink to="/auth/login" class="login__register">
+                    Sign in
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -259,33 +274,73 @@ const handleRegister = async () => {
 .login-page {
   display: flex;
   justify-content: center;
-  align-items: center;
   height: 100vh;
   background: #f3f4f6;
 }
 .login__wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 24px;
+  padding: 40px;
   width: 100%;
-  max-width: 465px;
+}
+.login__img {
   display: flex;
-  flex-direction: column;
   align-items: center;
-  gap: 80px;
+  justify-content: center;
+  flex-direction: column;
+  gap: 140px;
+  background: var(--blue);
+  border-radius: 24px;
+  box-shadow: 0px 0px 34.8px 10px #ffffff8f inset;
+}
+.login__type {
+  font-weight: 500;
+  font-style: Medium;
+  font-size: 24px;
+  line-height: 100%;
+  letter-spacing: 0%;
+  text-align: center;
+  text-transform: uppercase;
+  color: white;
+  letter-spacing: 1px;
+}
+.login__img img {
+  width: 480px;
+  height: auto;
+  object-fit: contain;
+  object-position: center;
+  mix-blend-mode: luminosity;
 }
 .login__body {
   width: 100%;
+  height: 100%;
   background: #ffffff;
   border-radius: 24px;
   padding: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 40px;
+}
+.login__somewhat {
+  min-width: 600px;
+  max-width: 90%;
+  margin: 0 auto;
+}
+.login__logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .login__logo img {
   width: 292px;
   height: 98px;
   object-fit: contain;
   object-position: center;
-  margin-top: -100px;
 }
 .login__header {
-  margin-bottom: 16px;
+  margin-bottom: 24px;
 }
 .login__title {
   font-weight: 600;
@@ -299,16 +354,16 @@ const handleRegister = async () => {
   line-height: 20px;
   color: var(--light-grey);
 }
-.login__form {
-  display: flex;
-  flex-direction: column;
+.login__items {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   gap: 16px;
+  margin-bottom: 24px;
 }
 .login__input {
   width: 100%;
   height: 48px;
   border-radius: 12px;
-  border: 1px solid var(--border);
 }
 .login__flexer {
   display: flex;
@@ -330,6 +385,7 @@ const handleRegister = async () => {
   display: flex;
   flex-direction: column;
   gap: 24px;
+  margin-top: 24px;
 }
 .login__btn {
   width: 100%;
